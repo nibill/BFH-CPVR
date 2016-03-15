e36b397cb4ed1f88f1e4ea76d743788cd56b806c
Thomas Buchegger <bucheggert@gmail.com>
2016-03-15 17:05:59 +0100

    Initial Load

---
+++ BillardExcercise/src/Billard_Tracker.java
@@ -0,0 +1,105 @@
+import java.awt.Color;
+import java.nio.ByteBuffer;
+
+import ij.ImagePlus;
+import ij.gui.NewImage;
+import ij.plugin.PNG_Writer;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.*;
+
+public class Billard_Tracker implements PlugInFilter
+{
+    @Override
+    public int setup(String arg, ImagePlus imp)
+    {   return DOES_8G;
+    }
+    
+    public static byte[] FloatArray2ByteArray(float[] values){
+        ByteBuffer buffer = ByteBuffer.allocate(4 * values.length);
+
+        for (float value : values){
+            buffer.putFloat(value);
+        }
+
+        return buffer.array();
+    }
+
+    @Override
+    public void run(ImageProcessor ip1)
+    {   int w1 = ip1.getWidth();
+        int h1 = ip1.getHeight();
+        byte[] pix1 = (byte[]) ip1.getPixels();
+        
+        ImagePlus imgGray = NewImage.createByteImage("GrayDeBayered", w1/2, h1/2, 1, NewImage.FILL_BLACK);
+        ImageProcessor ipGray = imgGray.getProcessor();
+        byte[] pixGray = (byte[]) ipGray.getPixels();
+        int w2 = ipGray.getWidth();
+        int h2 = ipGray.getHeight();
+        
+        ImagePlus imgRGB = NewImage.createRGBImage("RGBDeBayered", w1/2, h1/2, 1, NewImage.FILL_BLACK);
+        ImageProcessor ipRGB = imgRGB.getProcessor();
+        int[] pixRGB = (int[]) ipRGB.getPixels();
+        
+        long msStart = System.currentTimeMillis();
+        
+        ImagePlus imgHue = NewImage.createByteImage("Hue", w1/2, h1/2, 1, NewImage.FILL_BLACK);
+        ImageProcessor ipHue = imgHue.getProcessor();
+        byte[] pixHue = (byte[]) ipHue.getPixels();
+       
+        int i1 = 0, i2 = 0;
+        float[] hsbValues = new float[3];
+        
+        for (int y=0; y < h2; y++) 
+        {   
+            for (int x=0; x<w2; x++) 
+            {
+            	int G1 = pix1[i1] & 0xff;
+            	int G2 = pix1[i1 + w1 + 1] & 0xff;
+            	int green = (G1 + G2) >> 1;
+            	int blue = pix1[i1 + 1] & 0xff;
+            	int red = pix1[i1 + w1] & 0xff;
+            	
+            	hsbValues = Color.RGBtoHSB(red, green, blue, null);
+            	
+            	pixGray[y * w2 + x] = (byte) (hsbValues[2] * 255f);
+            	pixRGB[y * w2 + x] = ((red & 0xff)<<16)+((green & 0xff)<<8) + (blue & 0xff);
+            	pixHue[y * w2 + x] = (byte) (hsbValues[0] * 255f);
+            	
+            	i1 += 2;
+            }            
+            i1 += w1;
+        }
+        
+        long ms = System.currentTimeMillis() - msStart;
+        System.out.println(ms);
+        ImageStatistics stats = ipGray.getStatistics();
+        System.out.println("Mean:" + stats.mean);
+        
+        PNG_Writer png = new PNG_Writer();
+        try
+        {   png.writeImage(imgRGB , "../../Images/Billard1024x544x3.png",  0);
+            png.writeImage(imgHue,  "../../Images/Billard1024x544x1H.png", 0);
+            png.writeImage(imgGray, "../../Images/Billard1024x544x1B.png", 0);
+            
+        } catch (Exception e)
+        {   
+        	e.printStackTrace();
+        }
+        
+        imgGray.show();
+        imgGray.updateAndDraw();
+        imgRGB.show();
+        imgRGB.updateAndDraw();
+        imgHue.show();
+        imgHue.updateAndDraw();
+    }
+    
+    public static void main(String[] args)
+    {
+        Billard_Tracker plugin = new Billard_Tracker();
+        ImagePlus im = new ImagePlus("../../CPVR/CPVR/Images/Billard2048x1088x1.png");
+        im.show();
+        plugin.setup("", im);
+        plugin.run(im.getProcessor());
+    }
+}
