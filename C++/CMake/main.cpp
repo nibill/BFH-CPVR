97495d367ee8c97a126b3395ded80c433d3bae19
Thomas Buchegger <bucheggert@gmail.com>
2016-03-18 11:24:15 +0100

    C++ Project folder added

---
+++ C++/CMake/main.cpp
@@ -0,0 +1,14 @@
+// my first "Hello CMake"
+
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+// add your std::cout statement here
+
+	cout << "HELLO WORLD!";
+	return 0;
+}
